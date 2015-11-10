1e4b5754032cc2c91a3fada6d3bfe37a6c3c77e7
Jason <quizathaderat@yahoo.com>
2015-11-10 13:45:22 -0800

    Initial commit

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/cross_origin'
+require 'rack/utils'
+
+require 'pusher'
+require 'dotenv'
+
+Dotenv.load
+
+Pusher.app_id = ENV["PUSHER_APP_ID"]
+Pusher.key = ENV["PUSHER_APP_KEY"]
+Pusher.secret = ENV["PUSHER_APP_SECRET"]
+
+post '/messages' do
+  cross_origin
+  Pusher.trigger('messages', 'new_message', {
+    text: Rack::Utils.escape_html(@params["text"]),
+    username: Rack::Utils.escape_html(@params["username"]),
+    time: Rack::Utils.escape_html(@params["time"])
+  })
+end
